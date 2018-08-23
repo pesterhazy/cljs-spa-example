6d1debb60a09ce4233b83b643b83316bbeb6e617
Paulus Esterhazy <pesterhazy@gmail.com>
2018-08-23 18:18:52 +0200

    WIP

--- src/cljs_spa/core.cljs
+++ src/cljs_spa/core.cljs
@@ -1,13 +1,45 @@
 (ns cljs-spa.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [secretary.core :as secretary :refer-macros [defroute]]
+            [goog.events :as events]
+            [goog.history.EventType :as EventType])
+  (:import goog.History))
+
+(defonce !state (r/atom nil))
+
+(defn go-to [ky]
+  (swap! !state assoc :ky ky))
+
+(secretary/set-config! :prefix "#")
+
+(defroute home-path "/" []
+  (go-to :home))
+
+;; /#/users
+(defroute users-path "/users" []
+  (go-to :users))
+
+(defroute "*" []
+  (go-to :notfound))
+
+(defn inspector []
+  [:pre [:code (pr-str @!state)]])
 
 (defn main []
   [:main
    [:article
     [:h3 "Demo"]
-    [:p "Hello"]]])
+    [inspector]]])
 
 (defn render []
   (r/render [main] (.getElementById js/document "app")))
 
-(render)
+(def !history (delay (let [h (History.)]
+                       (goog.events/listen h EventType/NAVIGATE #(secretary/dispatch! (.-token %)))
+                       (doto h (.setEnabled true)))))
+
+(defn init []
+  (deref !history)
+  (render))
+
+(init)
