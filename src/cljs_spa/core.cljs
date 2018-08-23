0727a40aa8c697f2f89b6dd840b2019c85e4e76c
Paulus Esterhazy <pesterhazy@gmail.com>
2018-08-23 18:58:28 +0200

    WIP

--- src/cljs_spa/core.cljs
+++ src/cljs_spa/core.cljs
@@ -1,13 +1,13 @@
 (ns cljs-spa.core
   (:require [reagent.core :as r]
+            [clojure.string :as str]
             [accountant.core :as accountant]
             [bidi.bidi :as bidi]))
 
 (defonce !state (r/atom nil))
 
 (defn go-to [ky]
-  (js/console.warn "go-to" ky)
-  (swap! !state assoc :ky ky))
+  (swap! !state assoc :route ky))
 
 ;; ---
 
@@ -15,12 +15,17 @@
                      "asdf" :asdf}])
 
 (defn path-for [& args]
-  (str "#" (apply bidi.bidi/path-for my-routes args)))
+  (str "#" (apply bidi/path-for my-routes args)))
 
 (defn setup-router []
-  (js/console.log "setup-router")
-  (accountant/configure-navigation! {:nav-handler (fn [path] (js/console.warn "nav-handler" path))
-                                     :path-exists? (fn [path] (boolean (bidi/match-route my-routes path)))}))
+  (let [opts {:nav-handler
+              (fn [path]
+                (go-to (bidi/match-route my-routes
+                                         (-> path (str/split #"#" 2) last))))
+              :path-exists?
+              (fn [path]
+                (boolean (bidi/match-route my-routes path)))}]
+    (accountant/configure-navigation! opts)))
 ;; ---
 
 (defn inspector []
