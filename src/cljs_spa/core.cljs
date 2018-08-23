4e037e84caa56ce2b2d7f0993487b6466c13a77a
Paulus Esterhazy <pesterhazy@gmail.com>
2018-08-23 19:42:37 +0200

    WIP

--- src/cljs_spa/core.cljs
+++ src/cljs_spa/core.cljs
@@ -1,18 +1,36 @@
 (ns cljs-spa.core
   (:require [reagent.core :as r]
             [clojure.string :as str]
+            [clojure.pprint :refer [pprint]]
             [accountant.core :as accountant]
             [bidi.bidi :as bidi]))
 
 (defonce !state (r/atom nil))
 
-(defn go-to [ky]
-  (swap! !state assoc :route ky))
+(declare get-users)
+(declare get-user)
+
+(defn go-to [route]
+  (js/console.warn "go-to" route)
+  (swap! !state assoc :route route)
+  (let [{:keys [handler route-params]} route]
+    (case handler
+      :users
+      (get-users)
+      :user
+      (get-user route-params)
+      nil)))
+
+;; ---
+
+(defn inspector-ui []
+  [:pre [:code (with-out-str (pprint @!state))]])
 
 ;; ---
 
 (def my-routes ["/" {"" :home
-                     "asdf" :asdf}])
+                     "users" :users
+                     "users/" {[:id] :user}}])
 
 (defn path-for [& args]
   (str "#" (apply bidi/path-for my-routes args)))
@@ -26,16 +44,60 @@
               (fn [path]
                 (boolean (bidi/match-route my-routes path)))}]
     (accountant/configure-navigation! opts)))
+
 ;; ---
 
-(defn inspector []
-  [:pre [:code (pr-str @!state)]])
+(defn get-users []
+  (-> (js/fetch "https://reqres.in/api/users?page=1")
+      (.then #(.json %))
+      (.then (fn [js-data]
+               (swap! !state assoc :users-data (js->clj js-data :keywordize-keys true))))))
+
+(defn users-ui []
+  [:div
+   [:h3 "Users"]
+   (when-let [users-data (:users-data @!state)]
+     (->> users-data
+          :data
+          (map (fn [user]
+                 [:li [:a {:href (str "#/users/" (:id user))} (:first_name user)]]))
+          (into [:ul])))])
+
+;; ---
+
+(defn get-user [{:keys [id]}]
+  (-> (js/fetch (str "https://reqres.in/api/users/" id "?page=1"))
+      (.then #(.json %))
+      (.then (fn [js-data]
+               (swap! !state assoc :user-data (js->clj js-data :keywordize-keys true))))))
+
+(defn user-ui [route-params]
+  [:div
+   [:h3 "User"]
+   (when-let [user-data (:user-data @!state)]
+     [:div
+      [:img {:src (-> user-data :data :avatar)}]
+      (-> user-data :data :first_name)])])
+
+;; ---
+
+(defn not-found-ui []
+  [:div "Not Found"])
+
 
 (defn main []
-  [:main
-   [:article
-    [:h3 "Demo"]
-    [inspector]]])
+  [:div
+   [:main
+    [:article
+     (let [{:keys [handler route-params]} (-> @!state :route)]
+       (case handler
+         :users
+         [users-ui]
+         :user
+         [user-ui route-params]
+         [not-found-ui]))]]
+   [:footer
+    [inspector-ui]]])
 
 (defn render []
   (r/render [main] (.getElementById js/document "app")))
