862eca474076ada97358b5e369c95eb6edeb7c28
Paulus Esterhazy <pesterhazy@gmail.com>
2018-08-28 18:28:01 +0200

    Work around create-react-class issue

--- src/cljs_spa/core.cljs
+++ src/cljs_spa/core.cljs
@@ -1,5 +1,6 @@
 (ns cljs-spa.core
   (:require [reagent.core :as r]
+            [empty.empty]
             [cljs-spa.util :as util]
             [cljs-spa.router :as router]
             [cljs-spa.routes :as routes]))
