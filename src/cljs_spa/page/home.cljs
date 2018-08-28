90727c2824b82a00bd6ed0cf544db8896b3ba4d5
Paulus Esterhazy <pesterhazy@gmail.com>
2018-08-28 19:06:02 +0200

    Add example

--- src/cljs_spa/page/home.cljs
+++ src/cljs_spa/page/home.cljs
@@ -1,6 +1,20 @@
 (ns cljs-spa.page.home
   (:require [react-select :as react-select]))
 
+(def options
+  [{:value "simplicity" :label "simplicity"}
+   {:value "data" :label "data"}
+   {:value "lazy sequences" :label "lazy sequences"}])
+
+(defn selector-ui []
+  [:> (.-default react-select)
+   {:isMulti true
+    :options (clj->js options)}])
+
 (defn page-ui []
   [:h3 "Home"
-   [:> (.-default react-select)]])
+   [:div {:style {:margin-top 20
+                  :max-width 400}}
+    [:div {:style {:margin-bottom 20}} "What do you like?"]
+    [selector-ui]
+    ]])
