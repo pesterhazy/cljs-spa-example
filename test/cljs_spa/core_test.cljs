82c35b26375947678c4dbe38c39ecfaa35d3ed16
Paulus Esterhazy <pesterhazy@gmail.com>
2018-08-30 17:31:31 +0200

    Add test ns

--- test/cljs_spa/core_test.cljs
+++ test/cljs_spa/core_test.cljs
@@ -1,6 +1,8 @@
 (ns cljs-spa.core-test
-  (:require [clojure.test :refer [deftest testing is]]))
+  (:require [clojure.test :refer-macros [deftest testing is run-tests]]))
 
 (deftest hello-test
   (testing "hello"
     (is (= :foo :bar2))))
+
+(run-tests)
