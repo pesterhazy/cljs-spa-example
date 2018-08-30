19bbd6c31400131218f95c82b0ea55e19767d809
Paulus Esterhazy <pesterhazy@gmail.com>
2018-08-30 19:03:35 +0200

    Experiment with async tests

--- test/cljs_spa/core_test.cljs
+++ test/cljs_spa/core_test.cljs
@@ -1,6 +1,18 @@
 (ns cljs-spa.core-test
-  (:require [clojure.test :refer-macros [deftest testing is]]))
+  (:require [clojure.test :refer-macros [deftest testing is async
+                                         use-fixtures]]))
 
 (deftest hello-test
   (testing "hello"
-    (is (= :foo :bar))))
+    (is (= :foo :foo))))
+
+(deftest async-test
+  (async done
+         (-> (js/Promise.all [(js/Promise. (fn [_ reject]
+                                             (js/setTimeout #(reject (js/Error. "Timeout")) 500)))
+                              (js/Promise. (fn [resolve]
+                                             (js/setTimeout #(resolve (is (= 1 1))) 1000)))])
+             (.catch (fn [e]
+                       (js/console.error e)
+                       (is false (.-message e))))
+             (.finally done))))
--- test/cljs_spa/test_runner.cljs
+++ test/cljs_spa/test_runner.cljs
@@ -3,6 +3,10 @@
             [cljs-spa.core-test] ;; for side-effects
             [cljs-test-display.core :as td]))
 
+(defmethod clojure.test/report [:cljs-test-display.core/default :begin-test-var] [m]
+  (js/console.warn (clojure.test/testing-vars-str m))
+  (cljs-test-display.core/add-var-node m))
+
 (defn test-run []
   (run-tests (cljs-test-display.core/init! "app")
              'cljs-spa.core-test))
