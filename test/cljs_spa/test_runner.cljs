74dccf6315f78e744da1c41ceb3dae6b533498a2
Paulus Esterhazy <pesterhazy@gmail.com>
2018-08-31 11:05:51 +0200

    Clean up

--- test/cljs_spa/test_runner.cljs
+++ test/cljs_spa/test_runner.cljs
@@ -3,10 +3,6 @@
             [cljs-spa.core-test] ;; for side-effects
             [cljs-test-display.core :as td]))
 
-#_(defmethod clojure.test/report [:cljs-test-display.core/default :begin-test-var] [m]
-    (js/console.warn (clojure.test/testing-vars-str m))
-    (cljs-test-display.core/add-var-node m))
-
 (defn test-run []
   (run-tests (cljs-test-display.core/init! "app-test")
              'cljs-spa.core-test))
