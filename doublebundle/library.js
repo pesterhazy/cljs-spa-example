f4d91a9f821a6cbe933250ff1249a9c6fc146ce4
Paulus Esterhazy <pesterhazy@gmail.com>
2018-09-18 18:52:04 +0200

    Rename dependency

--- doublebundle/library.js
+++ doublebundle/library.js
@@ -3,5 +3,5 @@ window["ReactDOM"] = require("react-dom");
 window["createReactClass"] = require("create-react-class");
 window["router5"] = require("router5");
 window["router5/plugins/browser"] = require("router5/plugins/browser");
-window["noty"] = require("noty");
+window["Noty"] = require("noty");
 window["react-select"] = require("react-select");
